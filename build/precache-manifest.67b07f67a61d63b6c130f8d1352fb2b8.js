0f59daf64ca91c99fa1eff1079a71133401fa4a2
ajinkyaDivekar <ajinkyadivekar363@gmail.com>
2020-01-22 11:54:26 +0530

    update

---
+++ build/precache-manifest.67b07f67a61d63b6c130f8d1352fb2b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b74cf9d4648453e7779393e974dccb6",
+    "url": "/fifa-world-cup/index.html"
+  },
+  {
+    "revision": "f0e1d96fc0a537d9a785",
+    "url": "/fifa-world-cup/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "62c312121359bdd2a05b",
+    "url": "/fifa-world-cup/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0e1d96fc0a537d9a785",
+    "url": "/fifa-world-cup/static/js/2.0e717091.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/fifa-world-cup/static/js/2.0e717091.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62c312121359bdd2a05b",
+    "url": "/fifa-world-cup/static/js/main.b46dab2e.chunk.js"
+  },
+  {
+    "revision": "ee7ede07c309336272ff",
+    "url": "/fifa-world-cup/static/js/runtime-main.b8de2d00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fifa-world-cup/static/media/logo.5d5d9eef.svg"
+  }
+]);
